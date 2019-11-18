b4844d0a467323aadf68b48be3c5cff8220797c7
nukopy <pytwbf201830@gmail.com>
2019-11-18 20:39:44 +0900

    Modified CommitLog: get commit log from GitHub GraphQL API.

--- src/components/CommitLog.jsx
+++ src/components/CommitLog.jsx
@@ -1,43 +1,64 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
+import moment from "moment";
 
-// const CommitLog = () => {
-//   const {
-//     github: {
-//       repository: { ref: target }
-//     }
-//   } = useStaticQuery(graphql`
-//     {
-//       github {
-//         repository(owner: "nukopy", name: "wiki") {
-//           ref(qualifiedName: "master") {
-//             target {
-//               ... on GitHub_Commit {
-//                 id
-//                 history(first: 5) {
-//                   edges {
-//                     node {
-//                       id
-//                       message
-//                       committedDate
-//                       url
-//                       author {
-//                         name
-//                         email
-//                       }
-//                     }
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `);
-//   console.log(target);
+const CommitLog = () => {
+  const {
+    github: {
+      repository: {
+        ref: {
+          target: {
+            history: { edges: commits }
+          }
+        }
+      }
+    }
+  } = useStaticQuery(graphql`
+    {
+      github {
+        repository(owner: "nukopy", name: "wiki") {
+          ref(qualifiedName: "master") {
+            target {
+              ... on GitHub_Commit {
+                id
+                history(first: 5) {
+                  edges {
+                    node {
+                      id
+                      message
+                      committedDate
+                      url
+                      author {
+                        name
+                        email
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
 
-//   return <div>CommitLog.jsx</div>;
-// };
+  return (
+    <ul>
+      {commits.map(commit => {
+        return (
+          <li key={commit.node.id}>
+            <a href={commit.node.url} target="_blank">
+              {moment(commit.node.committedDate).format(
+                "MMM DD, YYYY hh:mm:ss"
+              )}{" "}
+              - {commit.node.message}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-// export default CommitLog;
+export default CommitLog;
