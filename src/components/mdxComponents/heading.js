18d6246e810d85b19682787cdb64374449eca52a
nukopy <pytwbf201830@gmail.com>
2020-01-18 04:21:53 +0900

    Modify Wiki's design.

--- src/components/mdxComponents/heading.js
+++ src/components/mdxComponents/heading.js
@@ -1,15 +1,19 @@
 import { system } from "styled-system";
 import styled from "@emotion/styled-base";
 
+const borderColor = 'rgb(147, 151, 153)';
+
 const Heading = {
   h1: styled("h1")({
     fortSize: [5, 6]
   }),
   h2: styled("h2")({
-    fontSize: 30
+    fontSize: 30,
+    borderBottom: `solid 2px black`
   }),
   h3: styled("h3")({
-    fontSize: 25
+    fontSize: 22.5,
+    borderBottom: `dashed 1.5px ${borderColor}`
   }),
   h4: styled("h4")({}),
   h5: styled("h5")({}),
