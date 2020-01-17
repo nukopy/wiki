b65a1d54552d18528af38fab499f8b56c91440f0
nukopy <pytwbf201830@gmail.com>
2020-01-18 02:57:20 +0900

    Add new mdx component for table of contents on Home screen.

---
+++ src/components/myMdxComponents/TableOfContents.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+
+import config from "../../../config";
+import Link from "../link";
+
+const isLocal = false;
+const host = isLocal ? "http://localhost:8000": "https://nukopy-wiki.netlify.com";
+
+const TableOfContents = ({ location }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allMdx {
+          edges {
+            node {
+              fields {
+                slug
+                title
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={({ allMdx }) => {
+      return (
+        <>
+          <ul>
+            {allMdx.edges.map(edge => {
+              const title = edge.node.fields.title;
+              const path = edge.node.fields.slug;
+              return (
+                <li>
+                  <Link to={host + path}>
+                    {title}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </>
+      );
+    }}
+  />
+);
+
+export default TableOfContents;
